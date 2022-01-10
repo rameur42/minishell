34ea4bb56cb225c99766e902b4ed6d520502ad8c
Reda AMEUR <rameur@e2r10p5.clusters.42paris.fr>
2022-01-10 16:43:55 +0100

    norm

---
+++ src/init_redirec.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_redirec.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rameur <rameur@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 16:25:56 by rameur            #+#    #+#             */
+/*   Updated: 2022/01/10 16:40:35 by rameur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+int	ft_print_red_error(char *s, int exit)
+{
+	printf("syntax error near unexpected token `%s'\n", s);
+	return (exit);
+}
+
+int	ft_init_3(t_list *tmp)
+{
+	if (tmp->prev && (tmp->prev->type != 0 && tmp->prev->type != 9
+		&& tmp->prev->type != 11 && tmp->prev->type != 2))
+		return (ft_print_red_error(tmp->prev->content, 1));
+	if (tmp->next && (tmp->next->type == 0 || tmp->next->type == 9
+			|| tmp->next->type == 11 || tmp->next->type == 2))
+	{
+		tmp->fd = open(tmp->next->content, O_TRUNC | O_WRONLY
+				| O_CREAT, 0644);
+		tmp->next->type = 2;
+		return (0);
+	}
+	else if (tmp->next)
+		return (ft_print_red_error(tmp->next->content, 1));
+	else
+		return (ft_print_red_error("newline", 1));
+	return (0);
+}
+
+int	ft_init_4(t_list *tmp)
+{
+	if (tmp->prev && (tmp->prev->type != 0 && tmp->prev->type != 9
+			&& tmp->prev->type != 11 && tmp->prev->type != 2))
+		return (ft_print_red_error(tmp->prev->content, 1));
+	if (tmp->next && (tmp->next->type == 0 || tmp->next->type == 9
+			|| tmp->next->type == 11 || tmp->next->type == 2))
+	{
+		tmp->fd = open(tmp->next->content, O_RDWR | O_CREAT | O_APPEND);
+		tmp->next->type = 2;
+		return (0);
+	}
+	else if (tmp->next)
+		return (ft_print_red_error(tmp->next->content, 1));
+	else
+		return (ft_print_red_error("newline", 1));
+}
+
+int	ft_init_5(t_list *tmp)
+{
+	
+}
