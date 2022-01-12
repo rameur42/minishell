3af8872842e0cd0d853f357d42d635c8d2849d47
Thomas GRESLE <tgresle@e2r10p5.clusters.42paris.fr>
2022-01-12 17:47:22 +0100

    norme env var

---
+++ src/env_var_norm.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_var_norm.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgresle <tgresle@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 17:26:02 by tgresle           #+#    #+#             */
+/*   Updated: 2022/01/12 17:31:50 by tgresle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+void	var_env_norm(t_list *tmp, t_struct *cfg)
+{
+	if (tmp->next)
+	{
+		tmp->next->prev = cfg->tenv;
+		cfg->tenv->pn = tmp->next->ps;
+	}
+	cfg->tenv->next = tmp->next;
+	free(tmp->content);
+	free(tmp);
+	tmp = cfg->tenv;
+	ft_f_arg(cfg);
+	cfg->tenv = NULL;
+}
