15280d7e3f4af63b15ad511d34586f81b43bf538
Reda AMEUR <rameur@e2r9p6.clusters.42paris.fr>
2022-01-14 15:36:53 +0100

    fixe d'un segfault

--- src/tokenize_env.c
+++ src/tokenize_env.c
@@ -6,7 +6,7 @@
 /*   By: rameur <rameur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 18:32:22 by rameur            #+#    #+#             */
-/*   Updated: 2022/01/14 13:14:31 by rameur           ###   ########.fr       */
+/*   Updated: 2022/01/14 15:35:38 by rameur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,6 +104,8 @@ int	set_var(t_struct *cfg, t_list *to_check)
 		{
 			buff = ft_substr(tmp->content,
 					ft_len_env(tmp->content, 1), ft_strlen(tmp->content));
+			if (buff == NULL)
+				return (0);
 			ft_tokenize_env(cfg, buff);
 			free(buff);
 			return (1);
